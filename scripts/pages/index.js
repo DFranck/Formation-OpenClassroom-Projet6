90ad933b1e035381aae045c867d3f7a94c286b7c
DFranck <franckdufournet@hotmail.fr>
2023-08-01 15:23:05 +0700

    fetch + display pohotographer

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,5 +1,50 @@
+/* eslint-disable quotes */
+function getPhotographer() {
+  const displayPhotographers = document.querySelector(
+    // eslint-disable-next-line comma-dangle
+    ".photographer_section"
+  );
+  // prettier me remet les double guillment a chaques save
+  fetch("../data/photographers.json")
+    .then((res) => res.json())
+    .then((data) => {
+      const photographersInfo = data.photographers.map((photographers) => {
+        const photographerName = photographers.name;
+        const photographerPic = `./assets/photographers/${photographers.portrait}`;
+        const photographerCity = photographers.city;
+        const photographerCountry = photographers.country;
+        const photographerTagline = photographers.tagline;
+        const photographerPrice = photographers.price;
+        const photographerId = photographers.id;
+        // console.log(photographers);
+        return {
+          photographerPic,
+          photographerName,
+          photographerCity,
+          photographerCountry,
+          photographerTagline,
+          photographerPrice,
+          photographerId,
+        };
+      });
+      photographersInfo.forEach((photographer) => {
+        displayPhotographers.innerHTML += `
+          <article class="photographer_card">
+            <img src="${photographer.photographerPic}" alt="Portrait de ${photographer.photographerName}"/>
+            <h2>${photographer.photographerName}</h2>
+            <h4>${photographer.photographerCity}, ${photographer.photographerCountry}</h4>
+            <p>${photographer.photographerTagline}</p>
+            <aside>${photographer.photographerPrice}€/jour</aside>
+          </article>
+        `;
+      });
+    });
+}
+
+getPhotographer();
 // async function getPhotographers() {
-//   // Ceci est un exemple de données pour avoir un affichage de photographes de test dès le démarrage du projet,
+// Ceci est un exemple de données pour avoir un
+// affichage de photographes de test dès le démarrage du projet,
 //   // mais il sera à remplacer avec une requête sur le fichier JSON en utilisant "fetch".
 //   let photographers = [
 //     {
