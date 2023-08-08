3e16c05f32d236a23a735e368551917526950c85
DFranck <franckdufournet@hotmail.fr>
2023-08-08 14:08:37 +0700

    insertion et modification du template

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -9,7 +9,7 @@ async function getPhotographers() {
 }
 
 function photographerTemplate(photographer) {
-  const { name, portrait } = photographer;
+  const { name, portrait, tagline, price, city, country } = photographer;
 
   const picture = `assets/photographers/${portrait}`;
 
@@ -19,8 +19,17 @@ function photographerTemplate(photographer) {
     img.setAttribute("src", picture);
     const h2 = document.createElement("h2");
     h2.textContent = name;
+    const p = document.createElement("p");
+    p.textContent = tagline;
+    const h3 = document.createElement("h3");
+    h3.textContent = `${city}, ${country}`;
+    const h4 = document.createElement("h4");
+    h4.textContent = `${price}/jour`;
     article.appendChild(img);
     article.appendChild(h2);
+    article.appendChild(h3);
+    article.appendChild(p);
+    article.appendChild(h4);
     return article;
   }
   return { name, picture, getUserCardDOM };
