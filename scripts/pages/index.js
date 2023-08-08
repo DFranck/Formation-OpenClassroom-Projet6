7d5d75fe0fd92e4a4d21d13cc1224c5fa73f5108
DFranck <franckdufournet@hotmail.fr>
2023-08-08 12:25:07 +0700

    reprise du code, rename + correction

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,26 +1,44 @@
+/* eslint-disable quotes */
 async function getPhotographers() {
   const response = await fetch("../../data/photographers.json");
   const { photographers } = await response.json();
+  console.log(photographers);
   return {
     photographers,
   };
 }
 
-// async function displayData(photographers) {
-//   const photographersSection = document.querySelector(".photographer_section");
+function photographerTemplate(photographer) {
+  const { name, portrait } = photographer;
 
-//   photographers.forEach((photographer) => {
-//     const photographerModel = photographerTemplate(photographer);
-//     const userCardDOM = photographerModel.getUserCardDOM();
-//     photographersSection.appendChild(userCardDOM);
-//   });
-// }
-// displayData();
+  const picture = `assets/photographers/${portrait}`;
+
+  function getUserCardDOM() {
+    const article = document.createElement("article");
+    const img = document.createElement("img");
+    img.setAttribute("src", picture);
+    const h2 = document.createElement("h2");
+    h2.textContent = name;
+    article.appendChild(img);
+    article.appendChild(h2);
+    return article;
+  }
+  return { name, picture, getUserCardDOM };
+}
+
+function displayPhotographers(photographers) {
+  const photographersSection = document.querySelector(".photographer_section");
+
+  photographers.forEach((photographer) => {
+    const photographerModel = photographerTemplate(photographer);
+    const userCardDOM = photographerModel.getUserCardDOM();
+    photographersSection.appendChild(userCardDOM);
+  });
+}
 
 async function init() {
-  // Récupère les datas des photographes
   const { photographers } = await getPhotographers();
-  // displayData(photographers);
+  displayPhotographers(photographers);
   console.log(photographers);
 }
 
