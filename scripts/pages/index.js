2b9000f27af993b0a855ae99190af34ef16edefa
DFranck <franckdufournet@hotmail.fr>
2023-08-08 17:04:38 +0700

    teste lien entre index et photographer.js

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
 /* eslint-disable object-curly-newline */
 /* eslint-disable quotes */
 async function getPhotographers() {
@@ -6,33 +7,6 @@ async function getPhotographers() {
   return {
     photographers,
   };
-}
-
-function photographerTemplate(photographer) {
-  const { name, portrait, tagline, price, city, country } = photographer;
-
-  const picture = `assets/photographers/${portrait}`;
-
-  function getUserCardDOM() {
-    const article = document.createElement("article");
-    const img = document.createElement("img");
-    img.setAttribute("src", picture);
-    const h2 = document.createElement("h2");
-    h2.textContent = name;
-    const p = document.createElement("p");
-    p.textContent = tagline;
-    const h3 = document.createElement("h3");
-    h3.textContent = `${city}, ${country}`;
-    const h4 = document.createElement("h4");
-    h4.textContent = `${price}/jour`;
-    article.appendChild(img);
-    article.appendChild(h2);
-    article.appendChild(h3);
-    article.appendChild(p);
-    article.appendChild(h4);
-    return article;
-  }
-  return { name, picture, getUserCardDOM };
 }
 
 function displayPhotographers(photographers) {
--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -1,5 +1,8 @@
-function photographerTemplate(data) {
-  const { name, portrait } = data;
+/* eslint-disable object-curly-newline */
+/* eslint-disable no-unused-vars */
+/* eslint-disable quotes */
+function photographerTemplate(photographer) {
+  const { name, portrait, tagline, price, city, country } = photographer;
 
   const picture = `assets/photographers/${portrait}`;
 
@@ -9,9 +12,18 @@ function photographerTemplate(data) {
     img.setAttribute("src", picture);
     const h2 = document.createElement("h2");
     h2.textContent = name;
+    const p = document.createElement("p");
+    p.textContent = tagline;
+    const h3 = document.createElement("h3");
+    h3.textContent = `${city}, ${country}`;
+    const h4 = document.createElement("h4");
+    h4.textContent = `${price}/jour`;
     article.appendChild(img);
     article.appendChild(h2);
+    article.appendChild(h3);
+    article.appendChild(p);
+    article.appendChild(h4);
     return article;
   }
-  return { name, picture, getUserCardDOM };
+  return { getUserCardDOM };
 }
