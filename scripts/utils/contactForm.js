0c9b9958dc9232d5b052bc72346e94fba6ddabf5
DFranck <franckdufournet@hotmail.fr>
2023-08-14 12:31:00 +0700

    contact modal ok

--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -1,9 +1,54 @@
-function displayModal() {
-    const modal = document.getElementById("contact_modal");
-	modal.style.display = "block";
+/* eslint-disable comma-dangle */
+/* eslint-disable operator-linebreak */
+/* eslint-disable implicit-arrow-linebreak */
+/* eslint-disable quotes */
+function targetPhotographer(photographers) {
+  const photographerOfThePage = photographers.find(
+    (photographer) =>
+      photographer.id.toString() ===
+      new URLSearchParams(window.location.search).get("id")
+  );
+  return photographerOfThePage;
+}
+async function getPhotographer() {
+  const response = await fetch("../../data/photographers.json");
+  const { photographers } = await response.json();
+  return targetPhotographer(photographers);
 }
 
-function closeModal() {
+const displayModalBtn = document.getElementById("open-contact-modal");
+displayModalBtn.addEventListener("click", () => {
+  async function displayModal() {
+    const photographerOfThePage = await getPhotographer();
+    const modal = document.getElementById("contact_modal");
+    modal.style.display = "block";
+    document.body.classList.add("backgroundBlur");
+    const { name, id } = photographerOfThePage;
+    const modalTitle = document.getElementById("contact-modal-title");
+    modalTitle.innerHTML = `Contactez-moi<br/>${name}`;
+    return id;
+  }
+  displayModal();
+});
+const closeModalBtn = document.getElementById("close-contact-modal");
+closeModalBtn.addEventListener("click", () => {
+  function closeModal() {
     const modal = document.getElementById("contact_modal");
     modal.style.display = "none";
+    document.body.classList.remove("backgroundBlur");
+  }
+  closeModal();
+});
+
+function submitForm(e) {
+  const inputsForm = e.target.querySelectorAll("input, textarea");
+  inputsForm.forEach((input) => {
+    const inputValue = input.value;
+    const inputName = input.id;
+    console.log(`${inputName} = ${inputValue}`);
+  });
 }
+document.querySelector("form").addEventListener("submit", (e) => {
+  e.preventDefault();
+  submitForm(e);
+});
