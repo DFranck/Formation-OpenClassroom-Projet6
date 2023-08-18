f1c1e4a5fc5267abd5a8edc3703f8b5dfae5e555
DFranck <franckdufournet@hotmail.fr>
2023-08-18 17:50:22 +0700

    fin a corriger

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -17,18 +17,14 @@ const popularitySortBtn = document.getElementById("popularity-sort-btn");
 const dateSortBtn = document.getElementById("date-sort-btn");
 const titleSortBtn = document.getElementById("title-sort-btn");
 const activeSortBtn = document.getElementById("active-sort-btn");
-let activeSort = popularitySortBtn.value;
 
 // ************************
-// *** FETCH et ISOLATION des DATA START***
+// *** FETCH et ISOLATION des DATA***
 // ************************
-async function getAllData() {
+
+async function getDataOfThePage() {
   const response = await fetch("../../data/photographers.json");
   const { media, photographers } = await response.json();
-  return { media, photographers };
-}
-async function getDataOfThePage() {
-  const { media, photographers } = await getAllData();
   const photographerOfThePage = photographers.find(
     (photographer) =>
       photographer.id.toString() ===
@@ -40,17 +36,39 @@ async function getDataOfThePage() {
   return { photographerOfThePage, mediasOfThePage };
 }
 // ************************
-// *** FETCH et ISOLATION des DATA END***
+// *** SORT ***
 // ************************
-
+async function sortGallery(
+  selectedSort,
+  photographerOfThePage,
+  mediasOfThePage
+) {
+  const gallery = document.querySelector(".gallery");
+  if (selectedSort === popularitySortBtn.value) {
+    mediasOfThePage.sort((a, b) => b.likes - a.likes);
+    sortMenu.setAttribute("aria-activedescendant", `popularity-sort-btn`);
+    popularitySortBtn.setAttribute("aria-selected", "true");
+  } else if (selectedSort === dateSortBtn.value) {
+    mediasOfThePage.sort((a, b) => new Date(b.date) - new Date(a.date));
+    sortMenu.setAttribute("aria-activedescendant", `date-sort-btn`);
+    dateSortBtn.setAttribute("aria-selected", "true");
+  } else if (selectedSort === titleSortBtn.value) {
+    mediasOfThePage.sort((a, b) => a.title.localeCompare(b.title));
+    sortMenu.setAttribute("aria-activedescendant", `title-sort-btn`);
+    titleSortBtn.setAttribute("aria-selected", "true");
+  }
+  gallery.innerHTML = "";
+  displayMediaOfThePage(selectedSort, photographerOfThePage, mediasOfThePage);
+}
 // ************************
-// *** DISPLAY START ***
+// *** DISPLAY ***
 // ************************
+/*
+DISPLAY GALLERY
+*/
 function displayPhotographerHeader(city, country, name, portrait, tagline) {
   const photographerinfo = document.querySelector(".photographer-infos");
   const photographerPortrait = document.querySelector(".photographer-portrait");
-  photographerinfo.innerHTML = "";
-  photographerPortrait.innerHTML = "";
   const h2 = document.createElement("h2");
   h2.textContent = name;
   const h3 = document.createElement("h3");
@@ -77,12 +95,16 @@ function displayPhotographerDetailsFooter(price, mediasOfThePage) {
   `;
 }
 function displayPhotographerOfThePage(photographerOfThePage, mediasOfThePage) {
-  const { city, country, id, name, portrait, price, tagline } =
+  const { city, country, name, portrait, price, tagline } =
     photographerOfThePage;
   displayPhotographerHeader(city, country, name, portrait, tagline);
   displayPhotographerDetailsFooter(price, mediasOfThePage);
 }
-function displayMediaOfThePage(mediasOfThePage) {
+function displayMediaOfThePage(
+  activeSort,
+  photographerOfThePage,
+  mediasOfThePage
+) {
   mediasOfThePage.forEach((galleryMedia) => {
     const gallery = document.querySelector(".gallery");
     const { title, likes, image, video, photographerId, id } = galleryMedia;
@@ -117,149 +139,17 @@ function displayMediaOfThePage(mediasOfThePage) {
     button.appendChild(figure);
     li.appendChild(button);
   });
-}
-async function displayDataOfThePage(photographerOfThePage, mediasOfThePage) {
-  displayPhotographerOfThePage(photographerOfThePage, mediasOfThePage);
-  displayMediaOfThePage(mediasOfThePage);
   handleLikes(photographerOfThePage, mediasOfThePage);
+  toggleSortMenu(activeSort, photographerOfThePage, mediasOfThePage);
   lightboxModalToggle(mediasOfThePage);
 }
-
-// ************************
-// *** DISPLAY END ***
-// ************************
-
-// ************************
-// *** SORT START ***
-// ************************
-async function sortGallery(selectedSort) {
-  const { photographerOfThePage, mediasOfThePage } = await getDataOfThePage();
-  const gallery = document.querySelector(".gallery");
-  if (selectedSort === popularitySortBtn.value) {
-    mediasOfThePage.sort((a, b) => b.likes - a.likes);
-    sortMenu.setAttribute("aria-activedescendant", `popularity-sort-btn`);
-    popularitySortBtn.setAttribute("aria-selected", "true");
-  } else if (selectedSort === dateSortBtn.value) {
-    mediasOfThePage.sort((a, b) => new Date(b.date) - new Date(a.date));
-    sortMenu.setAttribute("aria-activedescendant", `date-sort-btn`);
-    dateSortBtn.setAttribute("aria-selected", "true");
-  } else if (selectedSort === titleSortBtn.value) {
-    mediasOfThePage.sort((a, b) => a.title.localeCompare(b.title));
-    sortMenu.setAttribute("aria-activedescendant", `title-sort-btn`);
-    titleSortBtn.setAttribute("aria-selected", "true");
-  }
-  gallery.innerHTML = "";
-  displayDataOfThePage(photographerOfThePage, mediasOfThePage);
-}
-function toggleMenu() {
-  sortMenu.style.visibility = "visible";
-  activeSortBtn.setAttribute("aria-expanded", "true");
-}
-const sortMenuBtn = [popularitySortBtn, dateSortBtn, titleSortBtn];
-sortMenuBtn.forEach((btn) => {
-  btn.setAttribute("aria-selected", "false");
-  btn.addEventListener("click", () => {
-    activeSort = btn.value;
-    sortMenu.style.visibility = "hidden";
-    activeSortBtn.setAttribute("aria-expanded", "false");
-    activeSortBtn.innerHTML = `${activeSort} <i class="fa-solid fa-chevron-down"></i>`;
-    sortGallery(activeSort);
-  });
-});
-activeSortBtn.addEventListener("click", () => {
-  toggleMenu();
-});
-// ************************
-// *** SORT END ***
-// ************************
-// ************************
-// *** HANDLE LIKES START ***
-// ************************
-function handleLikes(photographerOfThePage, mediasOfThePage) {
-  const likes = document.querySelectorAll(".like-btn");
-  const gallery = document.querySelector(".gallery");
-  const asideContainer = document.getElementById("details-footer-container");
-  likes.forEach((btn) => {
-    btn.addEventListener("click", (e) => {
-      const whatMedia = mediasOfThePage.find(
-        (media) => media.id === Number(e.currentTarget.id)
-      );
-      e.stopPropagation();
-      whatMedia.likes += 1;
-      gallery.innerHTML = "";
-      asideContainer.innerHTML = "";
-      document.querySelector(".photographer-infos").innerHTML = "";
-      document.querySelector(".photographer-portrait").innerHTML = "";
-      displayDataOfThePage(photographerOfThePage, mediasOfThePage);
-    });
-  });
-}
-// ************************
-// *** HANDLE LIKES END ***
-// ************************
-// ************************
-// *** LIGHTBOX START ***
-// ************************
-function lightboxModalNav(currentIndex, mediasOfThePage) {
-  const previousMediaBtn = document.getElementById("lightbox-modal-previous");
-  const nextMediaBtn = document.getElementById("lightbox-modal-next");
-  const lightboxModal = document.getElementById("lightbox-modal");
-
-  previousMediaBtn.addEventListener("click", () => {
-    currentIndex -= 1;
-    const newCurrentIndex = currentIndex;
-    if (newCurrentIndex >= 0 && newCurrentIndex < mediasOfThePage.length) {
-      lightboxModalDisplay(newCurrentIndex, mediasOfThePage);
-    }
-  });
-  nextMediaBtn.addEventListener("click", () => {
-    currentIndex += 1;
-    const newCurrentIndex = currentIndex;
-    if (newCurrentIndex >= 0 && newCurrentIndex < mediasOfThePage.length) {
-      lightboxModalDisplay(newCurrentIndex, mediasOfThePage);
-    }
-  });
-  document.addEventListener("keydown", (e) => {
-    if (lightboxModal.style.display === "flex") {
-      switch (e.key) {
-        case "ArrowRight":
-          if (!(currentIndex === mediasOfThePage.length - 1)) {
-            currentIndex += 1;
-            const newCurrentIndex = currentIndex;
-            if (
-              newCurrentIndex >= 0 &&
-              newCurrentIndex < mediasOfThePage.length
-            ) {
-              lightboxModalDisplay(newCurrentIndex, mediasOfThePage);
-            }
-          }
-          break;
-        case "ArrowLeft":
-          if (!(currentIndex === 0)) {
-            currentIndex -= 1;
-            const newCurrentIndex = currentIndex;
-            if (
-              newCurrentIndex >= 0 &&
-              newCurrentIndex < mediasOfThePage.length
-            ) {
-              lightboxModalDisplay(newCurrentIndex, mediasOfThePage);
-            }
-          }
-          break;
-        case "Escape":
-          lightboxModalClose();
-          break;
-        default:
-          break;
-      }
-    }
-  });
-}
+/*
+DISPLAY LIGHTBOX
+*/
 function lightboxModalDisplay(currentIndex, mediasOfThePage) {
   let CurrentMediaTitle = "";
   let CurrentMediaContent = "";
   let lightboxModalMediaUrl = "";
-  const closeLightbox = document.getElementById("lightbox-modal-close");
   const previousMediaBtn = document.getElementById("lightbox-modal-previous");
   const nextMediaBtn = document.getElementById("lightbox-modal-next");
   const modalTitle = document.getElementById("modal-title");
@@ -298,51 +188,157 @@ function lightboxModalDisplay(currentIndex, mediasOfThePage) {
   modalMedia.appendChild(CurrentMediaContent);
   modalTitle.textContent = CurrentMediaTitle;
 }
-function lightboxModalClose() {
-  const lightboxModal = document.getElementById("lightbox-modal");
-  const asideContainer = document.getElementById("details-footer-container");
-  const modalMedia = document.getElementById("modal-media");
-  lightboxModal.style.display = "none";
-  asideContainer.style.visibility = "visible";
+// ************************
+// *** INTERACTIONS ***
+// ************************
+/*
+GESTION DU SORT
+*/
+function toggleSortMenu(activeSort, photographerOfThePage, mediasOfThePage) {
+  const sortMenuBtn = [popularitySortBtn, dateSortBtn, titleSortBtn];
+  function sortMenuOpen() {
+    sortMenu.style.visibility = "visible";
+    activeSortBtn.setAttribute("aria-expanded", "true");
+  }
+  function sortMenuClose() {
+    sortMenu.style.visibility = "hidden";
+    activeSortBtn.setAttribute("aria-expanded", "false");
+    activeSortBtn.innerHTML = `${activeSort} <i class="fa-solid fa-chevron-down"></i>`;
+    sortGallery(activeSort, photographerOfThePage, mediasOfThePage);
+  }
+  activeSortBtn.addEventListener("click", () => sortMenuOpen());
+  sortMenuBtn.forEach((btn) => {
+    btn.setAttribute("aria-selected", "false");
+    btn.addEventListener("click", () => {
+      activeSort = btn.value;
+      sortMenuClose();
+    });
+  });
 }
-function lightboxModalOpen(mediaId, mediasOfThePage) {
-  const lightboxModal = document.getElementById("lightbox-modal");
-  const asideContainer = document.getElementById("details-footer-container");
-  lightboxModal.style.display = "flex";
-  asideContainer.style.visibility = "hidden";
-  const currentIndex = mediasOfThePage.findIndex(
-    (media) => media.id === mediaId
-  );
-  lightboxModalDisplay(currentIndex, mediasOfThePage);
-  lightboxModalNav(currentIndex, mediasOfThePage);
+/*
+GESTION DES LIKES
+*/
+function handleLikes(photographerOfThePage, mediasOfThePage) {
+  const likesBtn = document.querySelectorAll(".like-btn");
+  const { price } = photographerOfThePage;
+  let whichMedia;
+  likesBtn.forEach((btn) => {
+    btn.addEventListener("click", (e) => {
+      if (!btn.hasAttribute("data-clicked")) {
+        whichMedia = mediasOfThePage.find(
+          (media) => media.id === Number(`${e.currentTarget.id}`)
+        );
+        whichMedia.likes += 1;
+        btn.innerHTML = `${whichMedia.likes} <i class="fa-solid fa-heart"></i>`;
+        e.stopPropagation();
+        displayPhotographerDetailsFooter(price, mediasOfThePage);
+        btn.setAttribute("data-clicked", "true");
+      }
+    });
+  });
 }
+/*
+GESTION DE LA LIGHTBOX
+*/
 function lightboxModalToggle(mediasOfThePage) {
   const cards = document.querySelectorAll(".card");
+  const closeLightbox = document.getElementById("lightbox-modal-close");
+  const lightboxModal = document.getElementById("lightbox-modal");
+  const asideContainer = document.getElementById("details-footer-container");
   let mediaId = "";
+  function lightboxModalOpen() {
+    lightboxModal.style.display = "flex";
+    asideContainer.style.visibility = "hidden";
+    const currentIndex = mediasOfThePage.findIndex(
+      (media) => media.id === mediaId
+    );
+    lightboxModalDisplay(currentIndex, mediasOfThePage);
+    lightboxModalNav(currentIndex, mediasOfThePage);
+  }
+  function lightboxModalClose() {
+    lightboxModal.style.display = "none";
+    asideContainer.style.visibility = "visible";
+  }
+  closeLightbox.addEventListener("click", () => {
+    lightboxModalClose();
+  });
+  document.addEventListener("keydown", (e) => {
+    if (e.key === "Escape") lightboxModalClose();
+  });
   cards.forEach((card) => {
     card.addEventListener("click", (e) => {
       mediaId = Number(e.currentTarget.id);
       lightboxModalOpen(mediaId, mediasOfThePage);
     });
   });
-  const closeLightbox = document.getElementById("lightbox-modal-close");
-  closeLightbox.addEventListener("click", () => {
-    lightboxModalClose();
+}
+
+function lightboxModalNav(currentIndex, mediasOfThePage) {
+  const previousMediaBtn = document.getElementById("lightbox-modal-previous");
+  const nextMediaBtn = document.getElementById("lightbox-modal-next");
+  const lightboxModal = document.getElementById("lightbox-modal");
+  /*
+CLICK NAVIGATION
+*/
+  previousMediaBtn.addEventListener("click", () => {
+    currentIndex -= 1;
+    const newCurrentIndex = currentIndex;
+    if (newCurrentIndex >= 0 && newCurrentIndex < mediasOfThePage.length) {
+      lightboxModalDisplay(newCurrentIndex, mediasOfThePage);
+    }
+  });
+  nextMediaBtn.addEventListener("click", () => {
+    currentIndex += 1;
+    const newCurrentIndex = currentIndex;
+    if (newCurrentIndex >= 0 && newCurrentIndex < mediasOfThePage.length) {
+      lightboxModalDisplay(newCurrentIndex, mediasOfThePage);
+    }
+  });
+  /*
+KEYBOARD NAVIGATION
+*/
+  document.addEventListener("keydown", (e) => {
+    if (lightboxModal.style.display === "flex") {
+      switch (e.key) {
+        case "ArrowRight":
+          if (!(currentIndex === mediasOfThePage.length - 1)) {
+            currentIndex += 1;
+            const newCurrentIndex = currentIndex;
+            if (
+              newCurrentIndex >= 0 &&
+              newCurrentIndex < mediasOfThePage.length
+            ) {
+              lightboxModalDisplay(newCurrentIndex, mediasOfThePage);
+            }
+          }
+          break;
+        case "ArrowLeft":
+          if (!(currentIndex === 0)) {
+            currentIndex -= 1;
+            const newCurrentIndex = currentIndex;
+            if (
+              newCurrentIndex >= 0 &&
+              newCurrentIndex < mediasOfThePage.length
+            ) {
+              lightboxModalDisplay(newCurrentIndex, mediasOfThePage);
+            }
+          }
+          break;
+        default:
+          break;
+      }
+    }
   });
 }
-// ************************
-// *** LIGHTBOX END ***
-// ************************
+
 // ************************
 // *** INIT START ***
 // ************************
 async function initialisation() {
   const { photographerOfThePage, mediasOfThePage } = await getDataOfThePage();
-  await displayDataOfThePage(photographerOfThePage, mediasOfThePage);
-
+  displayPhotographerOfThePage(photographerOfThePage, mediasOfThePage);
+  const activeSort = popularitySortBtn.value;
   activeSortBtn.innerHTML = `${activeSort} <i class="fa-solid fa-chevron-down"></i>`;
+  await sortGallery(activeSort, photographerOfThePage, mediasOfThePage);
 }
 initialisation();
-// ************************
-// *** INIT END ***
-// ************************
